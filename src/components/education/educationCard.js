63d70dfbdfccd93fccc77d62be836c3c3e969a27
Joseph Arbelaez <josepharbelaez@gmail.com>
2019-02-26 12:16:44 -0600

    Edu formatting and color change

--- src/components/education/educationCard.js
+++ src/components/education/educationCard.js
@@ -6,7 +6,6 @@ const EduCard = ({degree, institution, gpa, description, date, location}) => {
                 <div className = "bg-white v-base hover-bg-lightest-blue dib br3 pa3 ma2 grow bw2 shadow-5 tc">
                 <h3 classname = "eduDegree">{degree}</h3>
                     <h2 className = "eduCollege">{institution}</h2>
-                    <br />
                     <p className = "eduGPA">{gpa}</p>
                     <p className = "eduFeatures">{description}</p>
                     <p className = "expDate">{date}</p>
